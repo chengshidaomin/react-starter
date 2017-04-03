6af0f04150f8acc60caca4f941f4ba09936d6f6d
wanfan <hmrseeyou@gmail.com>
2017-04-03 17:45:57 +0800

    增加react类属性支持的bebal插件支持

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 # 将js/source目录下的js文件翻译后生成到js/build目录下
-babel --presets react,es2015 js/source -d js/build
+babel --presets react,es2015 --plugins transform-class-properties js/source -d js/build
 # 将翻译后的文件拼接起来，生成bundle.js
 browserify js/build/app.js -o bundle.js
 # 所有的css文件合并成bunlde.css
